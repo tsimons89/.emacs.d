e54e8747a341cf4118b37c683436af9146c2cf34
tsimons89 <taylorscottsimons@gmail.com>
2017-07-05 15:54:48 -0600

    Changes to org calendar prefs

--- init/behavior.el
+++ init/behavior.el
@@ -4,9 +4,20 @@
 (ido-mode t)
 (setq case-fold-search nil)
 (setq python-shell-completion-native-enable nil)
-(add-hook 'org-mode-hook 'turn-on-flyspell)
+
+
 (defun switch-to-terminal ()
+  "Switch to terminal buffer or create a new one if one does not already exist."
   (interactive)
   (if (get-buffer "*terminal*")
 	  (switch-to-buffer "*terminal*")
 	(ansi-term "/bin/bash" "terminal")))
+;; org mode
+
+(load-library "find-lisp")
+(setq org-agenda-files (find-lisp-find-files "~/org" "\.org$"))
+(add-hook 'org-mode-hook 'turn-on-flyspell)
+(defun my-org-agenda (&optional arg)
+  "Start agenda in specific mode."
+  (interactive)
+  (org-agenda arg "a"))
--- init/keybind.el
+++ init/keybind.el
@@ -2,3 +2,7 @@
 (global-set-key (kbd "<f5>") (kbd "C-u C-c C-c"))
 (global-set-key (kbd "<f12>") 'switch-to-terminal)
 (global-set-key "\M-\C-g" 'org-plot/gnuplot)
+
+;;org mode
+(global-set-key (kbd "<f9>") 'my-org-agenda)
+(define-key org-mode-map (kbd "C-c a") 'org-agenda)
