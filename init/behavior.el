b6cc59a7964bb007eb3da610829c7133b834d741
tsimons89 <taylorscottsimons@gmail.com>
2017-07-10 16:12:26 -0600

    Not sure what I added

--- init/behavior.el
+++ init/behavior.el
@@ -4,6 +4,12 @@
 (ido-mode t)
 (setq case-fold-search nil)
 (setq python-shell-completion-native-enable nil)
+;; do not add newline on semicolon
+(setq verilog-auto-newline nil)
+;; remove ";" mapping so that there are no electric effects
+(add-hook 'verilog-mode-hook
+  (lambda ()
+    (define-key verilog-mode-map ";" nil)))
 
 
 (defun switch-to-terminal ()
